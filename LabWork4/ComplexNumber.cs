d40251141f8a9d917eb955ed45f94ea2b43d37c9
Andrey Sharipov <andrey.sharipov1@gmail.com>
2023-11-24 10:59:27 +0200

    Добавьте файлы проекта.

---
+++ LabWork4/ComplexNumber.cs
@@ -0,0 +1,118 @@
+﻿namespace LabWork4
+{
+    internal class ComplexNumber
+    {
+        private double r;
+        private double fi;
+
+        public ComplexNumber(double r, double fi)
+        {
+            this.r = r;
+            this.fi = fi;
+        }
+
+        public ComplexNumber(int realPart, int imaginaryPart)
+        {
+            this.r = Math.Pow((Math.Pow(realPart, 2) + Math.Pow(imaginaryPart, 2)), 0.5);
+            this.fi = getArgComplexNumber(realPart, imaginaryPart);
+        }
+
+        public ComplexNumber Power(double n)
+        {
+            return new ComplexNumber(Math.Pow(r, n), n * fi);
+        }
+        
+        public override string ToString()
+        {
+            return $"{r} * (cos({fi}) + i * sin({fi}))";
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            // Преобразование к алгебраической форме и выполнение сложения
+            double aRe = a.r * Math.Cos(a.fi);
+            double aIm = a.r * Math.Sin(a.fi);
+            double bRe = b.r * Math.Cos(b.fi);
+            double bIm = b.r * Math.Sin(b.fi);
+
+            double resultRe = aRe + bRe;
+            double resultIm = aIm + bIm;
+
+            // Преобразование обратно в тригонометрическую форму
+            double resultP = Math.Sqrt(resultRe * resultRe + resultIm * resultIm);
+            double resultFi = Math.Atan2(resultIm, resultRe);
+
+            return new ComplexNumber(resultP, resultFi);
+        }
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            // Преобразование к алгебраической форме и выполнение вычитания
+            double aRe = a.r * Math.Cos(a.fi);
+            double aIm = a.r * Math.Sin(a.fi);
+            double bRe = b.r * Math.Cos(b.fi);
+            double bIm = b.r * Math.Sin(b.fi);
+
+            double resultRe = aRe - bRe;
+            double resultIm = aIm - bIm;
+
+            // Преобразование обратно в тригонометрическую форму
+            double resultP = Math.Sqrt(resultRe * resultRe + resultIm * resultIm);
+            double resultFi = Math.Atan2(resultIm, resultRe);
+
+            return new ComplexNumber(resultP, resultFi);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            // Выполнение умножения в тригонометрической форме
+            double resultP = a.r * b.r;
+            double resultFi = a.fi + b.fi;
+
+            return new ComplexNumber(resultP, resultFi);
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            // Выполнение деления в тригонометрической форме
+            double resultP = a.r / b.r;
+            double resultFi = a.fi - b.fi;
+
+            return new ComplexNumber(resultP, resultFi);
+        }
+
+        static public double getArgComplexNumber(double a, double b)
+        {
+            //Возвращает аргумент комплексного числа по таблице
+            if (a > 0 && b >= 0)
+            {
+                return Math.Atan(b / a);
+            }
+            else if (a < 0 && b >= 0)
+            {
+                return Math.PI - Math.Atan(Math.Abs(b / a));
+            }
+            else if (a < 0 && b < 0)
+            {
+                return Math.PI + Math.Atan(Math.Abs(b / a));
+            }
+            else if (a > 0 && b < 0)
+            {
+                return 2 * Math.PI - Math.Atan(Math.Abs(b / a));
+            }
+            else if (a == 0 && b > 0)
+            {
+                return Math.PI / 2;
+            }
+            else if (a == 0 && b < 0)
+            {
+                return (3 * Math.PI) / 2;
+            }
+            else
+            {
+                return b / a;
+            }
+        }
+
+
+    }
+}
---
+++ LabWork4/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace LabWork4
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Lab Work №4\n" + "Task 21");
+            Console.WriteLine("Create Complex Number №A:");
+            ComplexNumber complexNumberA = createComplexNumber();
+            Console.WriteLine("Create Complex Number №B:");
+            ComplexNumber complexNumberB = createComplexNumber();
+            Console.WriteLine("Create Complex Number №C:");
+            ComplexNumber complexNumberC = createComplexNumber();
+            Console.WriteLine("Create Complex Number №D:");
+            ComplexNumber complexNumberD = createComplexNumber();
+
+            Console.WriteLine("Result according to the formula: " +
+                (complexNumberB + complexNumberA - (complexNumberB / complexNumberC) * complexNumberD.Power(5)));
+        }
+
+        public static ComplexNumber createComplexNumber()
+        {
+            Console.WriteLine("Enter the real part of complex number: ");
+            int realPart = inputNumber();
+            Console.WriteLine("Enter the imaginary part of complex number without i: ");
+            int imaginaryPart = inputNumber();
+
+            return new ComplexNumber(realPart, imaginaryPart);
+        }
+
+        /*
+         * Метод для ввода числа с клавиатуры.
+         * В числе не должны присутствовать сторонние символы и число должно быть целое.
+         */
+        public static int inputNumber()
+        {
+            string input = "";
+            int number = 0;
+            while (true)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    number = int.Parse(input);
+
+                    return number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+        }
+    }
+}
